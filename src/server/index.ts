bdd2a18ba11b2facc3131ab78c9f860803e1f4c3
YongJie <viorama@gmail.com>
2020-04-13 14:49:48 +0800

    Shift cache-control up right before serving bundle

--- src/server/index.ts
+++ src/server/index.ts
@@ -55,6 +55,12 @@ initDb()
     // Initialise nodemailer
     initMailer()
 
+    // Site-wide cache control
+    app.use((_, res, next) => {
+      res.header('Cache-Control', 'no-store')
+      next()
+    })
+
     // To serve from build
     app.use(express.static('dist'))
 
@@ -87,7 +93,6 @@ initDb()
     // API configuration
     app.use((_, res, next) => {
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH')
-      res.header('Cache-Control', 'no-store')
       next()
     })
     app.use(bodyParser.urlencoded({ extended: false })) // application/x-www-form-urlencoded
