e69bc1b74fe58a36133f8ffba42301764eaddd34
YongJie <viorama@gmail.com>
2020-04-21 18:25:21 +0800

    Reformat pre-signed URL to one that is accepted by S3

--- src/server/util/aws.ts
+++ src/server/util/aws.ts
@@ -1,6 +1,22 @@
+import { parse } from 'url'
 import { S3 } from 'aws-sdk'
 
 export const s3 = new S3()
+
+/**
+ * Reformat the pre-signed url to to one that will be accepted by S3.
+ *
+ * This is necessary because we used a gov.sg CNAME and configured S3
+ * to only accept requests that have that gov.sg CNAME as the path.
+ *
+ * Broadly speaking, we need to change https://s3.aws.com/file.go.gov.sg/link?search
+ * to https://file.go.gov.sg/link?search.
+ */
+const reformatPresignedUrl = (url: string) => {
+  const urlObj = parse(url)
+  const { pathname, protocol, search } = urlObj
+  return `${protocol}//${pathname?.substring(1)}${search}`
+}
 
 export const generatePresignedUrl = async (fileName: string, fileType: string) => {
   const params = {
@@ -9,5 +25,6 @@ export const generatePresignedUrl = async (fileName: string, fileType: string) =
     ContentType: fileType,
     Expires: 60, // 60 seconds.
   }
-  return s3.getSignedUrlPromise('putObject', params)
+  const presignedUrl = await s3.getSignedUrlPromise('putObject', params)
+  return reformatPresignedUrl(presignedUrl)
 }
