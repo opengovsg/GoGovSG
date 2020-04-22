86b4e5826d8945bb7598d105d798a68ebbbac4f1
YongJie <viorama@gmail.com>
2020-04-22 16:36:23 +0800

    Update documentation

--- src/server/util/aws.ts
+++ src/server/util/aws.ts
@@ -9,8 +9,8 @@ export const s3 = new S3()
  * This is necessary because we used a gov.sg CNAME and configured S3
  * to only accept requests that have that gov.sg CNAME as the path.
  *
- * Broadly speaking, we need to change https://s3.aws.com/file.go.gov.sg/link?search
- * to https://file.go.gov.sg/link?search.
+ * Broadly speaking, we need to change https://s3.amazonaws.com/file.go.gov.sg/link?search
+ * to https://file.go.gov.sg.s3.amazonaws.com/link?search.
  */
 const reformatPresignedUrl = (url: string) => {
   const urlObj = parse(url)
