ca86c557d35065363f1874a16706a38309aee056
Kyler Wong <wsm.kyler@gmail.com>
2020-04-28 20:31:31 +0800

    removed legacy dead styles

--- src/client/components/BaseLayout/BaseLayoutHeader.jsx
+++ src/client/components/BaseLayout/BaseLayoutHeader.jsx
@@ -16,43 +16,6 @@ import Section from '../Section'
 
 const useStyles = makeStyles((theme) =>
   createStyles({
-    masthead: {
-      zIndex: '2',
-      position: 'relative',
-      backgroundColor: '#f0f0f0',
-      height: 'auto',
-      padding: '4px 0',
-      fontSize: '14px',
-    },
-    mastheadLink: {
-      marginLeft: '30px',
-      fontFamily: 'lato',
-      color: '#484848',
-      display: 'flex',
-      alignItems: 'center',
-      '&:hover': {
-        color: '#151515',
-      },
-    },
-    mastheadText: {
-      marginLeft: '4px',
-    },
-    mastheadIcon: {
-      fontSize: '20px',
-      fontFamily: 'sgds-icons !important',
-      speak: 'none',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      fontVariant: 'normal',
-      textTransform: 'none',
-      lineHeight: 1,
-      WebkitFontSmoothing: 'antialiased',
-      MozOsxFontSmoothing: 'grayscale',
-      '&:before': {
-        content: '"\\e948"',
-        color: '#db0000',
-      },
-    },
     appBar: {
       zIndex: '1',
       boxShadow: 'none',
