f3757295a6d32c5a28acf3681d667e49734341c9
Kyler Wong <wsm.kyler@gmail.com>
2020-05-13 13:32:17 +0800

    hostname fallback for invalid url

--- src/server/util/domain.ts
+++ src/server/util/domain.ts
@@ -3,12 +3,13 @@
  * @param urlToParse The url for its hostname to be extracted.
  */
 export default function parseDomain(urlToParse: string): string {
-  // Fallbacks to input url if it is not a valid url.
-  let parsedUrl = urlToParse
+  let parsedUrl
   try {
     const url = new URL(urlToParse)
     parsedUrl = url.hostname
-  } finally {
-    return parsedUrl
+  } catch {
+    // Fallbacks to input url if it is not a valid url.
+    parsedUrl = urlToParse
   }
+  return parsedUrl
 }
