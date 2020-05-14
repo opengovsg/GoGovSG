3c7567eb6f68a416e908d266a645594de44cd12c
YongJie <viorama@gmail.com>
2020-05-14 15:33:01 +0800

    Fix lint

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -2,7 +2,7 @@ export const DependencyIds = {
   urlCache: Symbol.for('urlCache'),
   urlRepository: Symbol.for('urlRepository'),
   analyticsLogging: Symbol.for('aLogging'),
-  cookieReducer: Symbol.for('cookieReducer')
+  cookieReducer: Symbol.for('cookieReducer'),
 }
 
 export default DependencyIds
