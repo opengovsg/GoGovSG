aa52e147a52a1b23a5e4c7289de7cb775cf17626
JasonChong96 <jasontjakra@gmail.com>
2020-05-18 13:03:40 +0800

    Improve logger messages for no-op mailer

--- src/server/util/emaildev.ts
+++ src/server/util/emaildev.ts
@@ -9,12 +9,11 @@ export class MailerNoOp implements Mailer {
   initMailer(): void {}
 
   mailOTP(_: string, __: string): Promise<void> {
-    logger.warn('Allowing user to OTP even though mail errored.')
-    logger.warn(
-      'This may be an issue with your IP. More information can be found at https://support.google.com/mail/answer/10336?hl=en)',
-    )
+    logger.warn('Allowing user to OTP regardless of input.')
     logger.warn('This message should NEVER be seen in production.')
 
     return Promise.resolve()
   }
 }
+
+export default MailerNoOp
