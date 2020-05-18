4abb55d034f602f283e5734a4a7233a2c1870a27
Kyler Wong <wsm.kyler@gmail.com>
2020-05-18 23:13:46 +0800

    added header, link analytics sections

---
+++ src/client/components/UserPage/Modals/ControlPanel/DialogHeader.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Typography, createStyles, makeStyles } from '@material-ui/core'
+
+import copyIcon from './assets/copy-icon.svg'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    dialogTitleDiv: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginTop: 116,
+      marginBottom: 44,
+    },
+    copyLinkDiv: {
+      display: 'flex',
+    },
+    copyIcon: {
+      marginRight: 5,
+    },
+  }),
+)
+
+export default function DialogHeader() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.dialogTitleDiv}>
+      <Typography variant="h2" color="primary">
+        Edit link
+      </Typography>
+      <div className={classes.copyLinkDiv}>
+        <img
+          className={classes.copyIcon}
+          src={copyIcon}
+          alt="Copy short link"
+        />
+        <Typography variant="subtitle2">Copy short link</Typography>
+      </div>
+    </div>
+  )
+}
