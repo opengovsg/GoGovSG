7ca3b061786ddd9cfae59bb73e2dbd81d1d72a66
Kyler Wong <wsm.kyler@gmail.com>
2020-05-19 03:44:46 +0800

    added trailing button placeholders

---
+++ src/client/components/UserPage/Modals/ControlPanel/assets/TrailingButton.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Button, createStyles, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    trailingButton: {
+      width: 135,
+      height: 44,
+    },
+  }),
+)
+
+type TrailingButtonProps = {
+  children: React.ReactNode
+  onClick:
+    | ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
+    | undefined
+}
+
+export default function TrailingButton(props: TrailingButtonProps) {
+  const classes = useStyles()
+  return (
+    <Button
+      className={classes.trailingButton}
+      variant="contained"
+      color="primary"
+      size="large"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </Button>
+  )
+}
