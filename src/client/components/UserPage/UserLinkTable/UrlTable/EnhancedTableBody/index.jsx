b0c35d4823b9842e40d3f513daf51d9389736745
Kyler Wong <wsm.kyler@gmail.com>
2020-05-20 17:49:17 +0800

    removed old components

--- src/client/components/UserPage/UserLinkTable/UrlTable/EnhancedTableBody/index.jsx
+++ src/client/components/UserPage/UserLinkTable/UrlTable/EnhancedTableBody/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import 'boxicons'
-// import PropTypes from 'prop-types'
-import { connect, useSelector } from 'react-redux'
-
+import { useSelector } from 'react-redux'
 import {
   Grid,
   Hidden,
@@ -17,24 +14,6 @@ import useAppMargins from '../../../../AppMargins/useAppMargins'
 import DrawerActions from '../../../Drawer/ControlPanel/helpers/reducers'
 import { useDrawerDispatch } from '../../../Drawer'
 import { numberUnitFormatter } from '../../../../../util/format'
-
-const mapDispatchToProps = (/* dispatch */) => ({
-  // openOwnershipModal: (shortUrl) =>
-  //   dispatch(userActions.openOwnershipModal(shortUrl)),
-  // toggleUrlState: (shortUrl, state) =>
-  //   dispatch(userActions.toggleUrlState(shortUrl, state)),
-  // onSaveUrl: (shortUrl, longUrl, editedLongUrl) => {
-  //   if (removeHttpsProtocol(longUrl) === editedLongUrl) {
-  //     dispatch(userActions.cancelEditLongUrl())
-  //   } else {
-  //     dispatch(userActions.updateLongUrl(shortUrl, editedLongUrl))
-  //   }
-  // },
-  // setEditedLongUrl: (shortUrl, editedLongUrl) => {
-  //   dispatch(userActions.setEditedLongUrl(shortUrl, editedLongUrl))
-  // },
-  // openQrCode: (shortUrl) => dispatch(userActions.openQrCode(shortUrl)),
-})
 
 const useStyles = makeStyles((theme) => {
   return createStyles({
@@ -165,32 +144,15 @@ const useStyles = makeStyles((theme) => {
   })
 })
 
-const EnhancedTableBody = (/* {
-  openOwnershipModal,
-  toggleUrlState,
-  onSaveUrl,
-  setEditedLongUrl,
-  openQrCode,
-} */) => {
+export default function EnhancedTableBody() {
   const urls = useSelector((state) => state.user.urls)
   const appMargins = useAppMargins()
   const classes = useStyles({ appMargins })
-
-  // Used to manage tooltip descriptions for our short url anchor button.
-  // const [isCopied, setCopied] = useState(false)
-  // const copiedLinkIconDesc = 'Link copied'
-
   const dispatch = useDrawerDispatch()
   const openControlPanel = (shortlink) =>
     dispatch({ type: DrawerActions.openControlPanel, payload: shortlink })
 
   if (urls.length > 0) {
-    // Text descriptions of icons buttons in user url table body.
-    // Used for tooltips and aria labels.
-    // const transferIconDesc = 'Transfer link'
-    // const copyLinkIconDesc = 'Copy link'
-    // const qrCodeIconDesc = 'Download QR Code'
-
     // If user has existing links, show the user's list of stored links.
     return (
       <TableBody className={classes.root}>
@@ -271,13 +233,3 @@ const EnhancedTableBody = (/* {
     </TableBody>
   )
 }
-
-EnhancedTableBody.propTypes = {
-  // openOwnershipModal: PropTypes.func.isRequired,
-  // toggleUrlState: PropTypes.func.isRequired,
-  // onSaveUrl: PropTypes.func.isRequired,
-  // setEditedLongUrl: PropTypes.func.isRequired,
-  // openQrCode: PropTypes.func.isRequired,
-}
-
-export default connect(null, mapDispatchToProps)(EnhancedTableBody)
