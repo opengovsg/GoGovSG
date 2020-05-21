795b31a1d422e93967e3b609d3f46342b3d5170d
YongJie <viorama@gmail.com>
2020-05-21 23:52:52 +0800

    Add documentation for endpoints

--- src/server/api/user.ts
+++ src/server/api/user.ts
@@ -131,7 +131,14 @@ function validateState(
 }
 
 /**
- * Endpoint for a user to create a short URL.
+ * Endpoint for a user to create a short URL. A short URL can either point to
+ * a long URL, or a file provided by the user.
+ *
+ * In order to create a file URL, provide the `isFile` parameter set to `true`, and supply
+ * the binary in a multipart/form-data under the field titled `file`. The longUrl
+ * parameter in the request body must point to the s3 bucket with the correct key. For
+ * example, a file upload request for the shortUrl named `test` should have `longUrl` set
+ * to `https://file[-staging].go.gov.sg/test`.
  */
 router.post('/url', fileUploadMiddleware, validateUrls, async (req, res) => {
   const { isFile, userId, longUrl, shortUrl } = req.body
@@ -227,7 +234,11 @@ router.patch('/url/ownership', async (req, res) => {
 })
 
 /**
- * Endpoint for user to edit a longUrl.
+ * Endpoint for user to edit a file or a longUrl.
+ *
+ * If editing a file link, only the file can be changed and not
+ * the long URL. This is to ensure a one-to-one mapping between
+ * the short URL and S3 object key.
  */
 router.patch(
   '/url/edit',
