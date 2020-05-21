f7270b743c4f81ef4f564e85564ad96730d5652f
JasonChong96 <jasontjakra@gmail.com>
2020-05-22 00:24:33 +0800

    Fix user page footer

---
+++ src/client/components/UserPage/UserLinkTable/UrlTable/MemoTablePagination/PaginationActionComponent/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Grid, IconButton, createStyles, makeStyles } from '@material-ui/core'
+import arrowLeftIcon from '../../../assets/arrow-left-icon.svg'
+import arrowRightIcon from '../../../assets/arrow-right-icon.svg'
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    pageSelectGrid: {
+      fontWeight: 500,
+      color: '#767676',
+      width: 'auto',
+      marginRight: '-139px',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: 'auto',
+      },
+    },
+  }),
+)
+
+export default ({ pageCount, onChangePage, page }) => {
+  const classes = useStyles()
+  return (
+    <Grid
+      container
+      item
+      alignItems="center"
+      spacing={5}
+      className={classes.pageSelectGrid}
+    >
+      <Grid item>
+        <IconButton
+          onClick={(event) => onChangePage(event, page - 1)}
+          disabled={page <= 0}
+        >
+          <img src={arrowLeftIcon} alt="Previous page" />
+        </IconButton>
+      </Grid>
+      <Grid item>{`Page ${page + 1} of ${pageCount}`}</Grid>
+      <Grid item>
+        <IconButton
+          onClick={(event) => onChangePage(event, page + 1)}
+          disabled={pageCount <= page + 1}
+        >
+          <img src={arrowRightIcon} alt="Next page" />
+        </IconButton>
+      </Grid>
+    </Grid>
+  )
+}
--- src/client/components/UserPage/UserLinkTable/UrlTable/MemoTablePagination/index.jsx
+++ src/client/components/UserPage/UserLinkTable/UrlTable/MemoTablePagination/index.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
-import { TablePagination } from '@material-ui/core'
+import { TablePagination, createStyles, makeStyles } from '@material-ui/core'
 import isMatch from 'lodash/isMatch'
+import useAppMargins from '../../../../AppMargins/appMargins'
+import PaginationActionComponent from './PaginationActionComponent'
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      display: 'inline',
+    },
+    toolbar: {
+      paddingLeft: 0,
+      paddingRight: 0,
+      width: '50%',
+    },
+    spacer: {
+      flex: 0,
+      paddingLeft: (props) => props.appMargins,
+    },
+    caption: {
+      fontWeight: 400,
+      marginRight: '4px',
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+    select: {
+      border: 'solid 1px #d8d8d8',
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+    selectIcon: {
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+  }),
+)
 
 // Prevents re-render if pagination did not change.
 const paginationInputIsEqual = (prev, next) =>
@@ -10,6 +47,9 @@ const paginationInputIsEqual = (prev, next) =>
 
 const MemoTablePagination = React.memo(
   ({ urlCount, tableConfig, updateUrlTableConfig }) => {
+    const appMargins = useAppMargins()
+    const classes = useStyles({ appMargins })
+
     const updateTableIfChanged = (newConfig) => {
       if (!isMatch(tableConfig, newConfig)) {
         updateUrlTableConfig(newConfig)
@@ -22,8 +62,18 @@ const MemoTablePagination = React.memo(
       updateTableIfChanged({ numberOfRows: event.target.value, pageNumber: 0 })
     }
 
+    const pageCount = Math.ceil(urlCount / tableConfig.numberOfRows)
+
     return (
       <TablePagination
+        ActionsComponent={({ onChangePage, page }) => (
+          <PaginationActionComponent
+            pageCount={pageCount}
+            onChangePage={onChangePage}
+            page={page}
+          />
+        )}
+        labelRowsPerPage="Links per page"
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
         count={urlCount}
@@ -37,6 +87,15 @@ const MemoTablePagination = React.memo(
         }}
         onChangePage={changePageHandler}
         onChangeRowsPerPage={changeRowsPerPageHandler}
+        classes={{
+          spacer: classes.spacer,
+          toolbar: classes.toolbar,
+          caption: classes.caption,
+          select: classes.select,
+          root: classes.root,
+          selectIcon: classes.selectIcon,
+        }}
+        labelDisplayedRows={() => {}}
       />
     )
   },
