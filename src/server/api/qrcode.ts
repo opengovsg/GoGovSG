75111b523e38294d0042b4d6bcf14a2d73e952dc
Kyler Wong <wsm.kyler@gmail.com>
2020-06-01 20:58:51 +0800

    Joi validation integration

--- src/server/api/qrcode.ts
+++ src/server/api/qrcode.ts
@@ -1,21 +1,45 @@
 import Express from 'express'
+import Joi from '@hapi/joi'
+import { createValidator } from 'express-joi-validation'
 
+import { ogUrl } from '../config'
 import createGoQrCode from '../util/qrcode'
 import ImageFormat from '../../shared/util/imageFormat'
+import { isValidShortUrl } from '../../shared/util/validation'
+
+function isValidFormat(format: string): boolean {
+  return (Object as any).values(ImageFormat).includes(format)
+}
+
+const qrCodeRequestSchema = Joi.object({
+  url: Joi.string()
+    .custom((url: string, helpers) => {
+      if (!isValidShortUrl(url)) {
+        return helpers.message({ custom: 'Not a valid short link' })
+      }
+      return url
+    })
+    .required(),
+  format: Joi.string()
+    .custom((format: string, helpers) => {
+      if (!isValidFormat(format)) {
+        return helpers.message({ custom: 'Not a valid format' })
+      }
+      return format
+    })
+    .required(),
+})
 
 const router = Express.Router()
+const validator = createValidator()
 
-router.post('/', (req, res) => {
+router.post('/', validator.body(qrCodeRequestSchema), (req, res) => {
   const url = req.body.url as string
   const format = req.body.format as ImageFormat
-  if (!url || !format) {
-    res
-      .status(400)
-      .send('Please include the required "url" and "format" headers')
-    return
-  }
+  // Append base url to short link before creating the qr.
+  const goShortLink = ogUrl + url
   // Creates the QR code and sends it to the client.
-  createGoQrCode(url, format).then((buffer) => {
+  createGoQrCode(goShortLink, format).then((buffer) => {
     res.contentType(format)
     res.end(buffer)
   })
