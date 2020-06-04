6b405258f284e579dd7cfd5fc7bb432aab91ee0e
JasonChong96 <jasontjakra@gmail.com>
2020-06-04 15:40:13 +0800

    Refactor and add tests for logout API

--- src/server/api/logout/handlers.ts
+++ src/server/api/logout/handlers.ts
@@ -1,14 +1,12 @@
 import Express from 'express'
-import jsonMessage from '../util/json'
+import jsonMessage from '../../util/json'
 
-const router = Express.Router()
-
-router.get('/', (req, res) => {
+export function logOut(req: Express.Request, res: Express.Response) {
   if (req.session) {
     req.session.destroy(() => res.ok(jsonMessage('Logged out')))
     return
   }
   res.serverError(jsonMessage('No session found'))
-})
+}
 
-export = router
+export default logOut
