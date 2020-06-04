ab2a8e74dcf99af2ce27d355cc8942a292098ce6
Kyler Wong <wsm.kyler@gmail.com>
2020-06-04 14:35:10 +0800

    Set default bucket as private

--- init-localstack.sh
+++ init-localstack.sh
@@ -2,5 +2,5 @@
 set -x
 pip install awscli-local
 awslocal s3 mb s3://$AWS_BUCKET_NAME
-awslocal s3api put-bucket-acl --bucket $AWS_BUCKET_NAME --acl public-read
+awslocal s3api put-bucket-acl --bucket $AWS_BUCKET_NAME --acl private
 set
