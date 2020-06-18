5ec5396d0a3b586acbbde635fdfa810f59db4033
JasonChong96 <jasontjakra@gmail.com>
2020-06-18 20:02:29 +0800

    feat: add new columns to api response

--- src/server/api/user/validators.ts
+++ src/server/api/user/validators.ts
@@ -2,6 +2,7 @@ import * as Joi from '@hapi/joi'
 import { ACTIVE, INACTIVE } from '../../models/types'
 import blacklist from '../../resources/blacklist'
 import { isHttps, isValidShortUrl } from '../../../shared/util/validation'
+import { LINK_DESCRIPTION_MAX_LENGTH } from '../../../shared/constants'
 
 export const urlRetrievalSchema = Joi.object({
   userId: Joi.number().required(),
@@ -51,6 +52,9 @@ export const urlEditSchema = Joi.object({
     file: Joi.object().keys().required(),
   }),
   state: Joi.string().allow(ACTIVE, INACTIVE).only(),
+  description: Joi.string().length(LINK_DESCRIPTION_MAX_LENGTH),
+  // TODO: Validator for email
+  contactEmail: Joi.string().required(),
 }).oxor('longUrl', 'files')
 
 export const ownershipTransferSchema = Joi.object({
--- src/server/repositories/types.ts
+++ src/server/repositories/types.ts
@@ -12,6 +12,8 @@ export type StorableUrl = Pick<
   | 'isFile'
   | 'createdAt'
   | 'updatedAt'
+  | 'description'
+  | 'contactEmail'
 >
 
 /**
