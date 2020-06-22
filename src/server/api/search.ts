e4b4d62a62b134598f39be51c796430bb00d1c20
JasonChong96 <jasontjakra@gmail.com>
2020-06-22 16:57:22 +0800

    feat: rate limiting on search endpoint

--- src/server/api/search.ts
+++ src/server/api/search.ts
@@ -1,6 +1,7 @@
 import Express from 'express'
 import { createValidator } from 'express-joi-validation'
 import Joi from '@hapi/joi'
+import rateLimit from 'express-rate-limit'
 import { container } from '../util/inversify'
 import { DependencyIds } from '../constants'
 import { SearchControllerInterface } from '../controllers/interfaces/SearchControllerInterface'
@@ -11,6 +12,11 @@ const urlSearchRequestSchema = Joi.object({
   offset: Joi.number(),
 })
 
+const apiLimiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 20,
+})
+
 const router = Express.Router()
 const validator = createValidator()
 const searchController = container.get<SearchControllerInterface>(
@@ -19,6 +25,7 @@ const searchController = container.get<SearchControllerInterface>(
 
 router.get(
   '/urls',
+  apiLimiter,
   validator.query(urlSearchRequestSchema),
   searchController.urlSearchPlainText,
 )
