bac49df3f4d3a7dfbe5b74381faf6eb84f97f166
Kyler Wong <wsm.kyler@gmail.com>
2020-06-26 00:45:22 +0800

    docs(stats): add int to weekday mapping to model

--- src/server/models/statistics/weekday.ts
+++ src/server/models/statistics/weekday.ts
@@ -30,6 +30,17 @@ export const WeekdayClicks = <WeekdayClicksTypeStatic>sequelize.define(
       type: Sequelize.INTEGER,
       primaryKey: true,
       validate: {
+        /**
+         * Number representing a day of the week.
+         *
+         * 0: Sunday.
+         * 1: Monday.
+         * 2: Tuesday.
+         * 3: Wednesday.
+         * 4: Thursday.
+         * 5: Friday.
+         * 6: Saturday.
+         */
         rangeCheck(day: number) {
           return day >= 0 && day < 7
         },
