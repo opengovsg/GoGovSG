b087d6dc299b73af6b5fad114f37ebcda28ad7c4
Kyler Wong <wsm.kyler@gmail.com>
2020-06-26 16:47:03 +0800

    feat(stats): add common bots check with tests included

--- src/server/services/DeviceCheckService.ts
+++ src/server/services/DeviceCheckService.ts
@@ -6,14 +6,18 @@ import {
   DeviceType,
 } from './interfaces/DeviceCheckServiceInterface'
 
+const BOTS_USER_AGENTS = /facebookexternalhit|Facebot|Slackbot|TelegramBot|WhatsApp|Twitterbot|Pinterest|Postman|url/
+
 @injectable()
 export class DeviceCheckService implements DeviceCheckServiceInterface {
   getDeviceType: (userAgent: string) => DeviceType = (userAgent) => {
     const parser = new UAParser(userAgent)
     const deviceType = parser.getDevice().type
 
-    // Desktop browsers do not identify themselves in user agent.
-    if (!deviceType) return 'desktop'
+    // Desktop browsers and bots do not get categorized by ua-parser.
+    if (!deviceType) {
+      return !userAgent.match(BOTS_USER_AGENTS) ? 'desktop' : 'others'
+    }
 
     // Possible types:
     // console, mobile, tablet, smarttv, wearable, embedded.
