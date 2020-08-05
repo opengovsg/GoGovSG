63fbc87fb937fce0ee7bd2eb216b9ce38b63f678
LoneRifle <LoneRifle@users.noreply.github.com>
2020-08-05 14:11:21 +0800

    chore(links): log user attempts to create malicious links

--- src/server/controllers/UrlCheckController.ts
+++ src/server/controllers/UrlCheckController.ts
@@ -6,6 +6,8 @@ import { UrlCheckControllerInterface } from './interfaces/UrlCheckControllerInte
 import { UrlCreationRequest } from '../../types/server/controllers/UserController'
 import { UrlThreatScanServiceInterface } from '../services/interfaces/UrlThreatScanServiceInterface'
 import { DependencyIds } from '../constants'
+import { logger } from '../config'
+import { UserType } from '../models/user'
 
 @injectable()
 export class UrlCheckController implements UrlCheckControllerInterface {
@@ -23,16 +25,21 @@ export class UrlCheckController implements UrlCheckControllerInterface {
     res: Response,
     next: NextFunction,
   ) => Promise<void> = async (req, res, next) => {
-    const { longUrl }: UrlCreationRequest = req.body
+    const { shortUrl, longUrl }: UrlCreationRequest = req.body
 
     if (longUrl) {
       try {
         const isThreat = await this.urlThreatScanService.isThreat(longUrl)
         if (isThreat) {
+          const user = req.session?.user as UserType
+          logger.warn(
+            `Malicious link attempt: User ${user?.id} tried to link ${shortUrl} to ${longUrl}`,
+          )
           res.badRequest(jsonMessage('Link is likely to be malicious.'))
           return
         }
       } catch (error) {
+        logger.error(error)
         res.serverError(jsonMessage(error.message))
         return
       }
