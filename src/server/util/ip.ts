aa0a7ab59b095315658a6273f352ac40c9bfaebc
Yong Jie <viorama@gmail.com>
2020-08-18 20:02:48 +0800

    refactor(otp): documentation

--- src/server/util/ip.ts
+++ src/server/util/ip.ts
@@ -3,7 +3,7 @@ import Express from 'express'
 /**
  * Extract client's IP address. Prioritise Cloudflare's client ip header, with
  * a fallback of req.ip for environments without Cloudflare proxy.
- * @param req
+ * @param req Express request.
  */
 export function getIp(req: Express.Request) {
   return req.get('CF-Connecting-IP') || req.ip
