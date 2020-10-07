70e9a3107995f0b403e18ab0d3dbacee3c154561
LoneRifle <LoneRifle@users.noreply.github.com>
2020-10-07 15:18:59 +0800

    chore(sql-fns): add isSearchable to url_histories insert

--- scripts/migrate_user_links.sql
+++ scripts/migrate_user_links.sql
@@ -14,6 +14,8 @@
 --   11 July 2019 Yuanruo Liang: Function created
 --   12 June 2020 Foo Yong Jie: Update function's url_history insertion step to include
 --                              compulsory isFile column
+--   07 Oct  2020 @LoneRifle: Update function's url_history insertion step to include
+--                            compulsory isSearchable column
 -- =============================================
 CREATE OR REPLACE FUNCTION migrate_user_links(from_user_email text, to_user_email text) RETURNS void AS
 $BODY$
@@ -39,8 +41,8 @@ BEGIN
 		RAISE EXCEPTION 'No transferring of links to the same user';
 	END IF;
 -- Insert the intended changes into URL history table
-    INSERT INTO url_histories ("urlShortUrl","longUrl","state","userId","isFile","createdAt","updatedAt")
-        SELECT "shortUrl", "longUrl", "state", "to_user_id", "isFile", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+    INSERT INTO url_histories ("urlShortUrl","longUrl","state","userId","isFile","isSearchable","createdAt","updatedAt")
+        SELECT "shortUrl", "longUrl", "state", "to_user_id", "isFile", "isSearchable", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
         FROM urls
         WHERE "userId" = from_user_id;
 -- Update the links in the URL table
