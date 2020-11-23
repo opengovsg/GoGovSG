6b7925a5e112a719e177639e6539258f81b52228
Yuanruo Liang <yuanruo@open.gov.sg>
2020-11-23 17:24:34 +0800

    feat: integrate redux-devtools into app

--- src/client/app/store.tsx
+++ src/client/app/store.tsx
@@ -1,7 +1,14 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+)
 
 export default store
