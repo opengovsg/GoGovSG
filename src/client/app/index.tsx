26ead3935f11d095ee700ab2bedd366119f3d7f6
Yuanruo Liang <yuanruo@open.gov.sg>
2020-11-23 12:04:02 +0800

    refactor(reducer): make root reducer explicit, convert file to TypeScript, prefer-default-exports

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -5,7 +5,7 @@ import { render } from 'react-dom'
 import Root from './components/pages/RootPage'
 import { get } from './util/requests'
 import './i18n'
-import { store } from './store'
+import store from './store'
 
 // If SENTRY_DNS env var is specified, init sentry.
 get('/api/sentry/').then((response) => {
--- src/client/app/store.tsx
+++ src/client/app/store.tsx
@@ -1,5 +1,7 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import reducer from './reducers'
+import rootReducer from './reducers'
 
-export const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export default store
