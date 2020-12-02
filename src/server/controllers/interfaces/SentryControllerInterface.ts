16640f0b62d9c6ef55b077bab9b5a5901391d96f
LoneRifle <LoneRifle@users.noreply.github.com>
2020-12-02 13:51:51 +0800

    refactor(sentry): move into module

--- src/server/controllers/interfaces/SentryControllerInterface.ts
+++
@@ -1,8 +0,0 @@
-import Express from 'express'
-
-export interface SentryControllerInterface {
-  /**
-   * Requests for the Sentry DNS.
-   */
-  getSentryDns(req: Express.Request, res: Express.Response): void
-}
