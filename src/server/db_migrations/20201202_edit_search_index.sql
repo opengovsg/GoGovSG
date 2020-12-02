6b5e5a6991777ef364b232bf705bb90c04ddb4ea
Oxiang <xiangqian_96@hotmail.com>
2020-12-02 11:16:59 +0800

    perf: include longurl to search
    - add longurl into search vector with B ranking
    - lower description ranking to C
    - include db migration script

---
+++ src/server/db_migrations/20201202_edit_search_index.sql
@@ -0,0 +1,19 @@
+-- This migration script changes the index for GoDirectory
+-- Include longUrl into the search index
+
+BEGIN TRANSACTION;
+
+DROP INDEX IF EXISTS urls_weighted_search_idx;
+
+-- Search will be run on a concatenation of vectors formed from short links, 
+-- long links and their description. Short link words can be taken as the 
+-- title and words there are likely to be more important. Long links are the
+-- alternative keyword searches done by users, as spotted in GA . The descriptions
+-- are no longer being filled by users and have the least importance.
+-- Search queries will have to use this exact expresion to be able to utilize the index.
+CREATE INDEX urls_weighted_search_idx ON urls USING gin ((setweight(to_tsvector(
+'english', urls."shortUrl"), 'A') || setweight(to_tsvector(
+'english', urls."longUrl"), 'B') || setweight(to_tsvector('english',
+urls."description"), 'C')));
+
+COMMIT;
