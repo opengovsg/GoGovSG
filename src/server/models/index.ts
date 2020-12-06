fc8c0a861152cd0fd7208249c2345205bec9cf5a
Yong Jie <viorama@gmail.com>
2020-12-06 18:01:48 +0800

    feat(url-clicks): add url clicks table

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -4,6 +4,7 @@ import { User } from './user'
 import { DailyClicks } from './statistics/daily'
 import { WeekdayClicks } from './statistics/weekday'
 import { Devices } from './statistics/devices'
+import { UrlClicks } from './statistics/clicks'
 import { syncFunctions } from './functions'
 
 // One user can create many urls but each url can only be mapped to one user.
@@ -18,9 +19,11 @@ UrlHistory.belongsTo(User, { foreignKey: { allowNull: false } })
 Url.hasMany(DailyClicks, { foreignKey: 'shortUrl', as: 'DailyClicks' })
 Url.hasMany(WeekdayClicks, { foreignKey: 'shortUrl', as: 'WeekdayClicks' })
 Url.hasOne(Devices, { foreignKey: 'shortUrl', as: 'DeviceClicks' })
+Url.hasOne(UrlClicks, { foreignKey: 'shortUrl', as: 'UrlClicks' })
 DailyClicks.belongsTo(Url, { foreignKey: 'shortUrl' })
 WeekdayClicks.belongsTo(Url, { foreignKey: 'shortUrl' })
 Devices.belongsTo(Url, { foreignKey: 'shortUrl' })
+UrlClicks.belongsTo(Url, { foreignKey: 'shortUrl' })
 
 /**
  * Initialise the database table.
---
+++ src/server/models/statistics/clicks.ts
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize'
+
+import { sequelize } from '../../util/sequelize'
+import { IdType } from '../../../types/server/models'
+
+export interface UrlClicksType extends IdType, Sequelize.Model {
+  readonly shortUrl: string
+  readonly clicks: number
+  readonly createdAt: string
+  readonly updatedAt: string
+}
+
+type UrlClicksTypeStatic = typeof Sequelize.Model & {
+  new (values?: object, options?: Sequelize.BuildOptions): UrlClicksType
+}
+
+export const UrlClicks = <UrlClicksTypeStatic>sequelize.define('url_clicks', {
+  shortUrl: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    validate: {
+      is: /^[a-z0-9-]+$/,
+    },
+  },
+  clicks: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+})
