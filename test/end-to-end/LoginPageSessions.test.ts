be551d8ab510fb40a0e63e3af6b1e837d5d8c7ad
Oxiang <xiangqian_96@hotmail.com>
2020-12-13 17:17:31 +0800

    refactor(eslint): add no extraneous dependencies

--- test/end-to-end/LoginPageSessions.test.ts
+++ test/end-to-end/LoginPageSessions.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { ClientFunction, Selector } from 'testcafe'
 import {
   incorrectEmail,
--- test/end-to-end/TransitionPage.test.ts
+++ test/end-to-end/TransitionPage.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { ClientFunction } from 'testcafe'
 import { rootLocation, shortUrl } from './util/config'
 import {
