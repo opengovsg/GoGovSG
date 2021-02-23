80931e91710bb6214997c3181114a79d4175f3ad
LoneRifle <LoneRifle@users.noreply.github.com>
2021-02-23 11:07:14 +0800

    feat(sentry): add to Redux

--- src/client/app/store.tsx
+++ src/client/app/store.tsx
@@ -2,12 +2,17 @@ import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import * as Sentry from '@sentry/react'
+
 import rootReducer from './reducers'
 
 const composeEnhancers = composeWithDevTools({
   trace: true,
 })
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk), Sentry.createReduxEnhancer()),
+)
 
 export default store
