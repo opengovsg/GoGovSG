653073301ab5d500ff306c4cc21896fb7d398edc
LoneRifle <LoneRifle@users.noreply.github.com>
2021-02-23 11:29:53 +0800

    refactor(sentry): integrate into react-router
    Replace HashRouter with Router and hash history to hook Sentry in

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import * as Sentry from '@sentry/react'
 import { render } from 'react-dom'
+
+import { createHashHistory } from 'history'
+
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 
 import Root from './components/pages/RootPage'
 import { get } from './util/requests'
 import './i18n'
 import store from './store'
+
+const history = createHashHistory()
 
 // If SENTRY_DNS env var is specified, init sentry.
 get('/api/sentry/').then((response) => {
@@ -14,10 +20,21 @@ get('/api/sentry/').then((response) => {
       if (sentryDns) {
         Sentry.init({
           dsn: sentryDns,
+          integrations: [
+            new Integrations.BrowserTracing({
+              routingInstrumentation: Sentry.reactRouterV5Instrumentation(
+                history,
+              ),
+            }),
+          ],
+          tracesSampleRate: 1.0,
         })
       }
     })
   }
 })
 
-render(<Root store={store} />, document.getElementById('root'))
+render(
+  <Root store={store} history={history} />,
+  document.getElementById('root'),
+)
