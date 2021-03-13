d8b2a10f4cb6099d54019d134de0dac376d4774a
Eugene Long <orbitalsqwib@gmail.com>
2021-03-14 01:37:21 +0800

    fix(user-controller): remove LongURLError type from error message
    Currently, tests expect a string with no type
    
    -remove LongURLError type to match expected results
    
    fix #1175

--- src/server/modules/user/UserController.ts
+++ src/server/modules/user/UserController.ts
@@ -77,7 +77,7 @@ export class UserController {
         return
       }
       logger.error(`Error creating short URL:\t${error}`)
-      res.badRequest(jsonMessage(error.message, MessageType.LongUrlError))
+      res.badRequest(jsonMessage(error.message))
       return
     }
   }
