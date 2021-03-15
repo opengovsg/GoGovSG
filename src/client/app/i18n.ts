7996933ccdeafbd8d7aca885b4ff904996635cc1
Eugene Long <orbitalsqwib@gmail.com>
2021-03-15 15:18:55 +0800

    build(public-assets): added dynamic localisations for i18n, dynamic title for template
    Currently, text assets are loaded through i18n. To reuse the existing functionality for the upcoming
    edu build, we need i18n to load in a different localisation file based on the `ASSET_VARIANT`
    environment variable.
    
    -injected `ASSET_VARIANT` env variable through webpack `DefinePlugin`
    configuration and implemented path switching at `i18n.ts`
    -implemented dynamic title for template
    used by `HTMLWebpackPlugin`

--- src/client/app/i18n.ts
+++ src/client/app/i18n.ts
@@ -2,12 +2,23 @@ import i18next from 'i18next'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
+declare let ASSET_VARIANT: string
+const assetVariant = ASSET_VARIANT || 'gov'
+
+const pathVariant =
+  assetVariant === 'edu'
+    ? '/edu/locales/{{lng}}/{{ns}}.json'
+    : '/gov/locales/{{lng}}/{{ns}}.json'
+
 export const i18nInit = i18next
   // load translation using http -> see /public/locales
   .use(Backend)
   .use(initReactI18next) // Passes i18n down to react-i18next.
   .init({
     // debug: true, // For verbose logging in development.
+    backend: {
+      loadPath: pathVariant,
+    },
     lng: 'en',
     fallbackLng: 'en',
     whitelist: ['en'],
