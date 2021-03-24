2ef715b7e44a533de7d9fe269e0b0736c95d1190
orbitalsqwib <21305518+orbitalsqwib@users.noreply.github.com>
2021-03-24 14:29:19 +0800

    refactor(redux-actions): refactor redux types to use generics (#1337)
    - Implement generic redux actions interfaces through intersection type extensions
    - Refactor redux type definitions to use generic interfaces
    - Define another type to save on the repetition of ampersands (DRY principle)
    
    Co-authored-by: LoneRifle <alwyn.tan@gmail.com>

--- src/client/home/actions/types.ts
+++ src/client/home/actions/types.ts
@@ -1,16 +1,17 @@
 import { HomeStatistics, LinksToRotate } from '../reducers/types'
+import { ReduxPayloadAction } from '../../app/actions/types'
 
 export const LOAD_STATS = 'LOAD_STATS'
 export const SET_LINKS_TO_ROTATE = 'SET_LINKS_TO_ROTATE'
 
-export type LoadStatsAction = {
-  type: typeof LOAD_STATS
-  payload: HomeStatistics
-}
+export type LoadStatsAction = ReduxPayloadAction<
+  typeof LOAD_STATS,
+  HomeStatistics
+>
 
-export type SetLinksToRotateAction = {
-  type: typeof SET_LINKS_TO_ROTATE
-  payload: LinksToRotate
-}
+export type SetLinksToRotateAction = ReduxPayloadAction<
+  typeof SET_LINKS_TO_ROTATE,
+  LinksToRotate
+>
 
 export type HomeActionType = SetLinksToRotateAction | LoadStatsAction
