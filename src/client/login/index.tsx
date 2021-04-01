85953dc98b8cea923152165880e3cf17aef85664
LoneRifle <LoneRifle@users.noreply.github.com>
2021-04-01 16:26:34 +0800

    fix(login): fill full window height if edu

--- src/client/login/index.tsx
+++ src/client/login/index.tsx
@@ -80,7 +80,8 @@ const useStyles = makeStyles((theme) =>
       backgroundColor: theme.palette.primary.dark,
       width: '50vw',
       height: '100%',
-      maxHeight: 'calc(100vh - 28px)',
+      maxHeight:
+        process.env.ASSET_VARIANT === 'edu' ? '100vh' : 'calc(100vh - 28px)',
       textAlign: 'center',
       overflow: 'hidden',
     },
@@ -230,7 +231,8 @@ const LoginPage: FunctionComponent<LoginPageProps> = ({
                       />
                     </Link>
                     <Typography className={classes.loginHeader} variant="body1">
-                      Only available for use by public officers with a{' '}
+                      Only available for use by{' '}
+                      {i18next.t('general.officerType')} officers with a{' '}
                       <strong>{i18next.t('general.emailDomain')}</strong> email.
                     </Typography>
                   </span>
