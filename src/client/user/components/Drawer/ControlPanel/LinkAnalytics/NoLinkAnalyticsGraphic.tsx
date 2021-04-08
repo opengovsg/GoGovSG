d2e0cb1a498debabf52b8a3cd2a4a60bba615e0c
Eugene Long <orbitalsqwib@gmail.com>
2021-04-08 17:07:13 +0800

    feature(drawer): add empty link analytics graphic

---
+++ src/client/user/components/Drawer/ControlPanel/LinkAnalytics/NoLinkAnalyticsGraphic.tsx
@@ -0,0 +1,46 @@
+import React, { FunctionComponent } from 'react'
+import { Typography, createStyles, makeStyles } from '@material-ui/core'
+import emptyGraphic from '@assets/components/user/drawer/empty-link-analytics-graphic.svg'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    emptyLinkAnalyticsGraphic: {
+      marginTop: '24px',
+      marginBottom: '142px',
+      position: 'relative',
+      zIndex: -1,
+    },
+    emptyLinkAnalyticsBodyText: {
+      marginTop: '27px',
+      textAlign: 'center',
+      padding: '10px',
+    },
+  }),
+)
+
+/**
+ * @component Default display component in place of search result.
+ */
+const NoLinkAnalyticsGraphic: FunctionComponent = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <Typography
+        variant="body1"
+        className={classes.emptyLinkAnalyticsBodyText}
+      >
+        Your link does not have any statistics yet
+      </Typography>
+      <div className={classes.emptyLinkAnalyticsGraphic}>
+        <img src={emptyGraphic} alt="empty link analytics graphic" />
+      </div>
+    </div>
+  )
+}
+
+export default NoLinkAnalyticsGraphic
