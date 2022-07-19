71716ff7f9cf845c5a9ddfe214f5da34bb1d916f
datle <thanhdat.toby@gmail.com>
2022-07-19 15:56:40 +0800

    feat: updated Docker and CI for Datadog integration

--- src/client/helpers/monitoring.ts
+++ src/client/helpers/monitoring.ts
@@ -1,8 +1,15 @@
 import { datadogRum } from '@datadog/browser-rum'
 
+if (
+  !process.env.DATADOG_APPLICATION_ID ||
+  !process.env.REACT_APP_DATADOG_CLIENT_TOKEN
+) {
+  return
+}
+
 datadogRum.init({
   applicationId: process.env.DATADOG_APPLICATION_ID,
-  clientToken: process.env.DATADOG_CLIENT_TOKEN,
+  clientToken: process.env.REACT_APP_DATADOG_CLIENT_TOKEN,
   site: 'datadoghq.com',
   service: 'gogovsg',
 
