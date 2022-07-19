05a48d7b4405de82f70a872253279e6343b7ba9f
datle <thanhdat.toby@gmail.com>
2022-07-19 16:10:47 +0800

    feat: updated Docker and CI for Datadog integration

--- src/client/helpers/monitoring.ts
+++ src/client/helpers/monitoring.ts
@@ -1,11 +1,11 @@
 import { datadogRum } from '@datadog/browser-rum'
 
-if (
-  !process.env.DATADOG_APPLICATION_ID ||
-  !process.env.REACT_APP_DATADOG_CLIENT_TOKEN
-) {
-  return
-}
+// if (
+//   !process.env.DATADOG_APPLICATION_ID ||
+//   !process.env.REACT_APP_DATADOG_CLIENT_TOKEN
+// ) {
+//   return
+// }
 
 datadogRum.init({
   applicationId: process.env.DATADOG_APPLICATION_ID,
