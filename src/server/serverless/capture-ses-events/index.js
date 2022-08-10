fa10bbeab4ada7a1789ed03e455dbc34e1d963d7
yongjie <viorama@gmail.com>
2022-08-10 17:58:27 +0000

    fix(ses): do logging on records level

--- src/server/serverless/capture-ses-events/index.js
+++ src/server/serverless/capture-ses-events/index.js
@@ -1,14 +1,17 @@
 const crossFetch = require('cross-fetch')
 
 async function handler(event) {
-  console.log(event)
-  const { SubscribeUrl } = event
-  console.log('subscribe url is', SubscribeUrl)
-  if (SubscribeUrl) {
-    await crossFetch(SubscribeUrl, {
-      method: 'GET',
-    })
-  }
+  event.Records.map((e) => {
+    console.log(e)
+    console.log(e.sns)
+    const { SubscribeUrl } = e
+    if (SubscribeUrl) {
+      return crossFetch(SubscribeUrl, {
+        method: 'GET',
+      })
+    }
+    return Promise.resolve()
+  })
 }
 
 module.exports.handler = handler
