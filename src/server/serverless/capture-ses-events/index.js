b32a57abbf4138172de88275b82ca8ea074dfd4e
yongjie <viorama@gmail.com>
2022-08-10 18:14:20 +0000

    feat(ses): remove subscription logic

--- src/server/serverless/capture-ses-events/index.js
+++ src/server/serverless/capture-ses-events/index.js
@@ -1,16 +1,6 @@
-const crossFetch = require('cross-fetch')
-
 async function handler(event) {
-  event.Records.map((e) => {
-    console.log(e)
-    console.log(e.sns)
-    const { SubscribeUrl } = e
-    if (SubscribeUrl) {
-      return crossFetch(SubscribeUrl, {
-        method: 'GET',
-      })
-    }
-    return Promise.resolve()
+  event.Records.forEach((e) => {
+    console.log(e.Sns)
   })
 }
 
