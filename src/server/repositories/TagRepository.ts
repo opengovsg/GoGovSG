a85568b156df05806f7536270475bda5c566c838
datle <thanhdat.toby@gmail.com>
2022-08-19 16:02:42 +0800

    feat: create tag API to include tags

--- src/server/repositories/TagRepository.ts
+++ src/server/repositories/TagRepository.ts
@@ -1,19 +1,7 @@
-// import { injectable } from 'inversify'
-// import { Mapper } from '../mappers/Mapper'
-// import { TagType } from '../models/tag'
-// import { DependencyIds } from '../constants'
 import { TagRepositoryInterface } from './interfaces/TagRepositoryInterface'
-// import { StorableTag } from './types'
-//
-// @injectable()
+
 export class TagRepository implements TagRepositoryInterface {
-  //   private tagMapper: Mapper<StorableTag, TagType>
-  //
-  //   public constructor(
-  //     @inject(DependencyIds.tagMapper) tagMapper: Mapper<StorableTag, TagType>,
-  //   ) {
-  //     this.tagMapper = tagMapper
-  //   }
+  // Placeholder for now
 }
-//
+
 export default TagRepository
