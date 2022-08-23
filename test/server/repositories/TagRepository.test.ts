6971bc249f7e09b9f4e903c92cc958335dda9fb9
datle <thanhdat.toby@gmail.com>
2022-08-23 11:36:32 +0800

    feat: update url API to include tags

--- test/server/repositories/TagRepository.test.ts
+++ test/server/repositories/TagRepository.test.ts
@@ -1,17 +1,29 @@
+import { Op } from 'sequelize'
 import { TagRepository } from '../../../src/server/repositories/TagRepository'
 import { tagModelMock } from '../api/util'
+import { TagMapper } from '../../../src/server/mappers/TagMapper'
 
-const repository = new TagRepository()
+jest.mock('../../../src/server/models/tag', () => ({
+  Tag: tagModelMock,
+}))
+
+const repository = new TagRepository(new TagMapper())
 describe('TagRepository', () => {
   const userId = 2
   const searchText = 'tag1'
 
   it('passes findTagsWithConditions through findAndCountAll', async () => {
     const conditions = { userId, searchText, limit: 5 }
-    const findAndCountAll = jest.spyOn(tagModelMock, 'findAndCountAll')
+    const findAndCountAll = jest.spyOn(tagModelMock, 'findAll')
     await expect(
       repository.findTagsWithConditions(conditions),
     ).resolves.toBeNull()
-    expect(findAndCountAll).toHaveBeenCalledWith({ where: { searchText } })
+    expect(findAndCountAll).toHaveBeenCalledWith({
+      where: {
+        tagString: {
+          [Op.like]: `${conditions.searchText}%`,
+        },
+      },
+    })
   })
 })
