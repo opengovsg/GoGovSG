f2e10479297ffe697cbc2484eec3252854fc5b55
datle <thanhdat.toby@gmail.com>
2022-10-08 11:00:54 +0800

    feat: apiKeyAuthService and apiKeyAuthMiddleware

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,16 @@
 import Express from 'express'
 import jsonMessage from '../util/json'
-import { ERROR_404_PATH } from '../constants'
+import { DependencyIds, ERROR_404_PATH } from '../constants'
 import { displayHostname } from '../config'
 import assetVariant from '../../shared/util/asset-variant'
+import { container } from '../util/inversify'
+import ApiKeyAuthService from '../modules/user/services/ApiKeyAuthService'
 
+const BEARER_STRING = 'Bearer'
+const BEARER_SEPARATOR = ' '
+const apiKeyAuthService = container.get<ApiKeyAuthService>(
+  DependencyIds.apiKeyAuthService,
+)
 const router = Express.Router()
 
 /*  Public routes that do not need to be protected */
@@ -31,6 +38,38 @@ function userGuard(
 }
 
 /**
+ * To protect external APIs by APIKey.
+ * */
+async function apiKeyAuthMiddleware(
+  req: Express.Request,
+  res: Express.Response,
+  next: Express.NextFunction,
+) {
+  const authorizationHeader = req.headers.authorization
+  if (!authorizationHeader) {
+    res.unauthorized(jsonMessage('Authorization header is missing'))
+    return
+  }
+  const [bearerString, apiKey] = authorizationHeader.split(BEARER_SEPARATOR)
+  if (bearerString !== BEARER_STRING) {
+    res.unauthorized(jsonMessage('Invalid authorization header format'))
+    return
+  }
+  try {
+    const user = await apiKeyAuthService.getUserByApiKey(apiKey)
+    if (!user) {
+      res.unauthorized(jsonMessage('Invalid API Key'))
+      return
+    }
+    req.body.userId = user.id
+    next()
+  } catch {
+    res.unauthorized(jsonMessage('Invalid API Key'))
+    return
+  }
+}
+
+/**
  *  Preprocess request parameters.
  * */
 function preprocess(
@@ -51,6 +90,9 @@ router.use('/qrcode', userGuard, require('./qrcode'))
 router.use('/link-stats', userGuard, require('./link-statistics'))
 router.use('/link-audit', userGuard, require('./link-audit'))
 router.use('/directory', userGuard, require('./directory'))
+
+/* Register APIKey protected endpoints */
+router.use('/v1', apiKeyAuthMiddleware, preprocess, require('./external'))
 
 router.use((_, res) => {
   res.status(404).render(ERROR_404_PATH, {
