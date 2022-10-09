3581de58b1d8e26ea78bf2863fe242c82fbfb4ad
Jim Engay <e0425954@u.nus.edu>
2022-10-10 01:00:20 +0800

    feat: create sqs in localstack

--- init-localstack.sh
+++ init-localstack.sh
@@ -3,4 +3,7 @@ set -x
 pip install awscli-local
 awslocal s3 mb s3://$AWS_BUCKET_NAME
 awslocal s3api put-bucket-acl --bucket $AWS_BUCKET_NAME --acl private
+
+awslocal sqs create-queue --queue-name $AWS_SQS_BULK_QRCODE_GENERATE_START
+awslocal sqs list-queues
 set
