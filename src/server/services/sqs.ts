aa3081693b82aabe0beab4df868bb9ab7522137d
Jim Engay <e0425954@u.nus.edu>
2022-10-12 00:09:59 +0800

    chore: tidy up sqs service and add filePath to sendMessage function

--- src/server/services/sqs.ts
+++ src/server/services/sqs.ts
@@ -4,39 +4,32 @@ import { DependencyIds } from '../constants'
 import { sqsBulkQRCodeStartUrl } from '../config'
 import { BulkUrlMapping } from '../repositories/types'
 
-export interface SQSInterface {
-  sendMessage(mappings: BulkUrlMapping[]): Promise<void>
+export interface SQSServiceInterface {
+  sendMessage(filePath: string, mappings: BulkUrlMapping[]): Promise<void>
 }
 
 @injectable()
-export class SQSService implements SQSInterface {
+export class SQSService implements SQSServiceInterface {
   private sqsClient: SQS
 
   constructor(@inject(DependencyIds.sqsClient) sqsClient: SQS) {
     this.sqsClient = sqsClient
   }
 
-  sendMessage: (mappings: BulkUrlMapping[]) => Promise<void> = async (
-    mappings,
-  ) => {
-    await this.sqsClient.sendMessage(
-      {
-        MessageAttributes: {
-          Mappings: {
-            DataType: 'String',
-            StringValue: JSON.stringify(mappings),
-          },
+  sendMessage: (filePath: string, mappings: BulkUrlMapping[]) => Promise<void> =
+    async (filePath, mappings) => {
+      await this.sqsClient.sendMessage(
+        {
+          MessageBody: JSON.stringify({ filePath, mappings }),
+          QueueUrl: sqsBulkQRCodeStartUrl,
         },
-        MessageBody: 'This is a message',
-        QueueUrl: sqsBulkQRCodeStartUrl,
-      },
-      (err, data) => {
-        if (err) {
-          console.log(`SQS sendMessage error: ${err}`)
-        } else {
-          console.log(`success ${data.MessageId}, mappings: ${data}`)
-        }
-      },
-    )
-  }
+        (err, data) => {
+          if (err) {
+            console.log(`SQS sendMessage error: ${err}`)
+          } else {
+            console.log(`SQS sendMessage success, messageId: ${data.MessageId}`)
+          }
+        },
+      )
+    }
 }
