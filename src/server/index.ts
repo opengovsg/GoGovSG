7d38caae73be1f9cd9d290ec7cdb78fee6f8ce11
Den <denesh@u.nus.edu>
2022-10-18 22:18:17 +0800

    feat: json parsing error

--- src/server/index.ts
+++ src/server/index.ts
@@ -223,6 +223,17 @@ initDb()
         res.badRequest(jsonMessage(err.error.toString()))
         return
       }
+
+      if (
+        err instanceof SyntaxError &&
+        err.statusCode === 400 &&
+        'body' in err &&
+        err.type === 'entity.parse.failed'
+      ) {
+        console.error(err)
+        res.badRequest(jsonMessage('Bad Request. JSON is malformed'))
+      }
+
       res.status(500).render('500.error.ejs')
       return
     }
