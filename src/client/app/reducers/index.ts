1f47e82b6ebe750413a95957515952893f1c2c48
datle <thanhdat.toby@gmail.com>
2022-11-08 14:40:57 +0800

    feat: implemented API Key Page

--- src/client/app/reducers/index.ts
+++ src/client/app/reducers/index.ts
@@ -4,7 +4,7 @@ import user from '../../user/reducers'
 import root from '../components/pages/RootPage/reducers'
 import home from '../../home/reducers'
 import directory from '../../directory/reducers'
-import { api } from '../../apiintegration/reducers'
+import api from '../../apiintegration/reducers'
 
 const rootReducer = combineReducers({
   login,
