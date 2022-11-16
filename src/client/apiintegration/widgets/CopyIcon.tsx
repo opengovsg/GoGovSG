3751935b01d11e0aa973046da8b5fb7221469d48
thanhdatle <thanhdat.toby@gmail.com>
2022-11-16 11:32:21 +0800

    Update src/client/apiintegration/widgets/CopyIcon.tsx
    Co-authored-by: halfwhole <41856541+halfwhole@users.noreply.github.com>

--- src/client/apiintegration/widgets/CopyIcon.tsx
+++ src/client/apiintegration/widgets/CopyIcon.tsx
@@ -11,9 +11,9 @@ const CopyIcon: FunctionComponent<CopyIconProps> = ({
 }: CopyIconProps) => {
   return (
     <svg
-      width="26"
-      height="20"
-      viewBox="0 0 26 20"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       style={{
