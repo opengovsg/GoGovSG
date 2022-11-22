b106c83348385d89bb77aee3df8c94858b11c016
Alexis <gweiying@gmail.com>
2022-11-22 19:31:41 +0800

    chore: pass download urls to Download QR code button

--- src/client/user/components/UserLinkTable/DownloadBulkButton/index.tsx
+++ src/client/user/components/UserLinkTable/DownloadBulkButton/index.tsx
@@ -4,56 +4,49 @@ import * as Sentry from '@sentry/react'
 import { GAEvent } from '../../../../app/util/ga'
 import { DropdownButton, DropdownOption } from '../../../widgets/DropdownButton'
 
-// TODO: add bucket path, ideally we want to store this as an env variable and import it
-const BUCKET_PATH = ''
-
 type DownloadFormats = 'csv' | 'png' | 'svg'
 
 type DownloadPathMap = { [format in DownloadFormats]: string }
 
 const downloadPaths: DownloadPathMap = {
-  csv: 'urls.csv',
-  png: 'qr_code_png.zip',
-  svg: 'qr_code_svg.zip',
+  csv: 'generated.csv',
+  png: 'generated_png.zip',
+  svg: 'generated_svg.zip',
 }
 
-function downloadFileFromS3(
-  format: DownloadFormats,
-  bulkCsvId: string,
-  bucketPath: string = BUCKET_PATH,
-) {
-  // TODO: exact functionality to be confirmed
-  try {
-    window.open(`${bucketPath}/${bulkCsvId}/${downloadPaths[format]}`)
-    GAEvent(`qr code bulk download`, format, 'successful')
-  } catch (e) {
-    Sentry.captureMessage(`qr code bulk download for ${format} unsuccessful`)
-    GAEvent('qr code bulk download', format, 'unsuccesful')
-  }
+function downloadFileFromS3(format: DownloadFormats, bulkCsvIds: string[]) {
+  bulkCsvIds.forEach((bulkCsvId) => {
+    try {
+      window.open(`${bulkCsvId}/${downloadPaths[format]}`)
+      GAEvent(`qr code bulk download`, format, 'successful')
+    } catch (e) {
+      Sentry.captureMessage(`qr code bulk download for ${format} unsuccessful`)
+      GAEvent('qr code bulk download', format, 'unsuccesful')
+    }
+  })
 }
 
-export default function DownloadBulkButton() {
-  const bulkCsvId = 'placeholder_id' // TODO: likely will be retrieved from Redux store
+const DownloadBulkButton = ({ bulkCsvIds }: { bulkCsvIds: string[] }) => {
   const options: DropdownOption[] = [
     {
       name: 'Download all',
       onClick: () => {
-        downloadFileFromS3('csv', bulkCsvId)
-        downloadFileFromS3('png', bulkCsvId)
-        downloadFileFromS3('svg', bulkCsvId)
+        downloadFileFromS3('csv', bulkCsvIds)
+        downloadFileFromS3('png', bulkCsvIds)
+        downloadFileFromS3('svg', bulkCsvIds)
       },
     },
     {
       name: 'Links (CSV File)',
-      onClick: () => downloadFileFromS3('csv', bulkCsvId),
+      onClick: () => downloadFileFromS3('csv', bulkCsvIds),
     },
     {
       name: 'QR Codes (PNG)',
-      onClick: () => downloadFileFromS3('png', bulkCsvId),
+      onClick: () => downloadFileFromS3('png', bulkCsvIds),
     },
     {
       name: 'QR Codes (SVG)',
-      onClick: () => downloadFileFromS3('svg', bulkCsvId),
+      onClick: () => downloadFileFromS3('svg', bulkCsvIds),
     },
   ]
 
@@ -61,3 +54,5 @@ export default function DownloadBulkButton() {
     <DropdownButton buttonText="Download .zip" options={options} fullWidth />
   )
 }
+
+export default DownloadBulkButton
