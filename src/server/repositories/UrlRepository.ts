c9da75b862fd8e7e337ae900a9fc38b349120191
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-08-20 20:57:40 +0800

    feat: start writing RedirectionDestination in redis cache (#2407)

--- src/server/repositories/UrlRepository.ts
+++ src/server/repositories/UrlRepository.ts
@@ -208,10 +208,8 @@ export class UrlRepository implements UrlRepositoryInterface {
       } catch {
         // Cache failed, look in database
         const redirectDestination = await this.getLongUrlFromDatabase(shortUrl)
-        // FIXME: Cache the entire RedirectDestination object once all app
-        // clients are updated to use the new RedirectDestination type.
-        this.cacheShortUrl(shortUrl, redirectDestination.longUrl).catch(
-          (error) => logger.error(`Unable to cache short URL: ${error}`),
+        this.cacheShortUrl(shortUrl, redirectDestination).catch((error) =>
+          logger.error(`Unable to cache short URL: ${error}`),
         )
         return redirectDestination
       }
@@ -562,35 +560,23 @@ export class UrlRepository implements UrlRepositoryInterface {
    * @param  {string} shortUrl Short url.
    * @param  {string} longUrl Long url.
    */
-  private cacheShortUrl: (shortUrl: string, longUrl: string) => Promise<void> =
-    (shortUrl, longUrl) => {
-      return new Promise((resolve, reject) => {
-        redirectClient.set(shortUrl, longUrl, 'EX', redirectExpiry, (err) => {
+  private cacheShortUrl: (
+    shortUrl: string,
+    redirectDestination: RedirectDestination,
+  ) => Promise<void> = (shortUrl, redirectDestination) => {
+    return new Promise((resolve, reject) => {
+      redirectClient.set(
+        shortUrl,
+        JSON.stringify(redirectDestination),
+        'EX',
+        redirectExpiry,
+        (err) => {
           if (err) reject(err)
           else resolve()
-        })
-      })
-    }
-
-  // FIXME: This is the future implementation of the cacheShortUrl method to be
-  // used once all app clients are updated to use the new RedirectDestination
-  // private cacheShortUrl: (
-  //   shortUrl: string,
-  //   redirectDestination: RedirectDestination,
-  // ) => Promise<void> = (shortUrl, redirectDestination) => {
-  //   return new Promise((resolve, reject) => {
-  //     redirectClient.set(
-  //       shortUrl,
-  //       JSON.stringify(redirectDestination),
-  //       'EX',
-  //       redirectExpiry,
-  //       (err) => {
-  //         if (err) reject(err)
-  //         else resolve()
-  //       },
-  //     )
-  //   })
-  // }
+        },
+      )
+    })
+  }
 
   /**
    * Generates the ranking algorithm to be used in the ORDER BY clause in the
