0308e06eaec5c46a2c24ce0d44431a0dbbd60ae5
seaerchin <jiachin@open.gov.sg>
2025-12-23 14:43:46 +0800

    chore: update variable to use shared const

--- config/config.js
+++ config/config.js
@@ -1,34 +1,14 @@
+const config = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+}
 export default {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
-  staging: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
+  development: config,
+  staging: config,
+  production: config,
+  test: config,
 }
