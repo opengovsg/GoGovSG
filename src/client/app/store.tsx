69056259110a3e5b786c7da4bd5e06ce4fd81dac
tomaioo <hongsaygio.com@gmail.com>
2026-07-25 07:23:29 -0700

    fix(security): 2 improvements across 2 files
    - Security: Hardcoded Datadog API credentials in client-side code
    - Security: Redux DevTools enabled with trace in production
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- src/client/app/store.tsx
+++ src/client/app/store.tsx
@@ -1,12 +1,13 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './reducers'
 
-const composeEnhancers = composeWithDevTools({
-  trace: true,
-})
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools({ trace: true })
+    : compose
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
